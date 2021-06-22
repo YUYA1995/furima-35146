a74c1985e343331728185a8c23910922e936e47b
YUYA1995 <82224371+YUYA1995@users.noreply.github.com>
2021-06-22 15:01:57 +0900

    4回目商品詳細表示機能編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'homes/index'
   devise_for :users
   root to: 'items#index'
-  resources :users ,only: [:index] 
   resources :items ,only: [:new, :create, :show]
 
 end
