36020c4b373dfb3c3e0334c365a88fd0c9a15166
YUYA1995 <82224371+YUYA1995@users.noreply.github.com>
2021-06-24 16:30:44 +0900

    1回目商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.order("id DESC")
@@ -18,21 +18,24 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def edit
-  #   def edit
-  #     @item = Item.find(params[:id])
-  #     unless user_signed_in? && current_user.id == @item.user_id
-  #       redirect_to action: :index
-  #     end
-  #   end
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+       redirect_to root_path
+     else
+       render :edit
+     end
+  end
 
   def show
     @item = Item.find(params[:id])
   end
 
   private
-
   def item_params
     params.require(:item).permit(:title, :product_price, :category_id, :address_id, :condition_id, :postage_id, :deliver_day_id, :explain, :image).merge(user_id: current_user.id)
   end
